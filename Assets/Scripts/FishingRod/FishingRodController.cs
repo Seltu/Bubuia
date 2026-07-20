4e7a6daffe74fd795c8288d0890d5bfd9a2c56a8
Seltu <sndm.snf22@uea.edu.br>
2026-07-20 12:42:52 -0400

    Fishing bugfixes

--- Assets/Scripts/FishingRod/FishingRodController.cs
+++ Assets/Scripts/FishingRod/FishingRodController.cs
@@ -370,7 +370,7 @@ public class FishingRodController : MonoBehaviour
 
         float currentDist = dirXZ.magnitude;
 
-        float bias = (_hookingDistance - currentDist) * 20f;
+        float bias = (_hookingDistance - currentDist) * 40f;
         float jitter = (Random.value - 0.5f) * 20f;
 
         float forward = (bias + jitter) * Time.deltaTime;
@@ -429,7 +429,7 @@ public class FishingRodController : MonoBehaviour
 
     private void HookingDistanceUpdate(int currentScore, int maxScore)
     {
-        _hookingDistance = 30f - 40f * ((float)currentScore / maxScore);
+        _hookingDistance = 30f - 25f * ((float)currentScore / maxScore);
     }
 
     private void EndHooking(bool caught)
